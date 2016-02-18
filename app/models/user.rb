66905c076fce402eaafb13fb0b1f9f9bb671d441
slava-nikulin <unicorninthecorn@gmail.com>
2016-02-18 17:54:31 +0300

    fixed roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,23 @@
 class User < ActiveRecord::Base
-	belongs_to :role
 	has_many :orders, :dependent => :delete_all
+	has_and_belongs_to_many :roles
 	devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 	before_create :set_role, :create_api_token
 	validates :name, presence: true, length: { maximum: 30 }
 
 	def admin?
-		self.role.name == "lunch admin"
+		has_role?("lunch admin")
 	end
 
 	private
 
+	def has_role?(*role_names)
+		self.roles.where(:name => role_names).present?
+	end
+
 	def set_role
-		self.role = Role.find_by(name: User.count == 0 ? "lunch admin" : "customer")
+		self.roles << Role.find_by(name: "customer")
+		self.roles << Role.find_by(name: "lunch admin") if User.count == 0	
 	end
 
 	def create_api_token
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213160010) do
+ActiveRecord::Schema.define(version: 20160218143316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20160213160010) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles_users", id: false, force: :cascade do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
+  add_index "roles_users", ["role_id"], name: "index_roles_users_on_role_id", using: :btree
+  add_index "roles_users", ["user_id"], name: "index_roles_users_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
@@ -69,13 +77,10 @@ ActiveRecord::Schema.define(version: 20160213160010) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "role_id"
     t.string   "api_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
-  add_foreign_key "users", "roles"
 end
